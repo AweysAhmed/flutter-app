7bc67216e85e164b52caa5cb5f7316e26b609cd8
OFFICE <office@OFFICEs-MacBook-Air-2.local>
2020-01-19 16:38:41 -0500

    working on flutter app

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  void futureValueResult() {
+    setState(() {
+      //function to calculate futre value
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.green[700]),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("How much did that really cost?"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                  "Oh. You need a little dummy text for your mockup? How quaint. I bet you’re still using Bootstrap too…"),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'What did you buy?',
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'How much did it cost?',
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'How many years in the future?',
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Estimated Rate of Return',
+                ),
+              ),
+              RaisedButton(
+                child: Text("Calculate"),
+                onPressed: null,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
